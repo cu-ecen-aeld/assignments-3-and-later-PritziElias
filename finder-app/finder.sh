4702ac9b34e4d21ecec690d104d2598ca45cfde4
eli <pritzielias@gmail.com>
2024-10-11 16:20:12 +0200

    Initial Setup

---
+++ finder-app/finder.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Read in two runtime arguments
+# First one -> path to a directory on the filesystem: filesdir
+# Second one -> text string which will be searched for: searchstr
+
+# Check if both arguments exist and throw an error with value 1 if not
+
+# Check if filesdir is a directory and throw an error of value 1 if not
+
+# Do the according search and write the output.
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,8 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* my_username_from_autotest = my_username();
+    const char* my_username_from_conf = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING(my_username_from_autotest, my_username_from_conf);
+    // TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 }
